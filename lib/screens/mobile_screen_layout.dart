f8d3cbd4df265a3727b1f6858dc7d7d294dc9070
Ahmed Ali <sadakc15@gmail.com>
2023-12-06 14:53:10 +0300

    last update

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -2,55 +2,96 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/widgets/contacts_lists.dart';
 
+
 class MobileScreenLayout extends StatelessWidget {
-  const MobileScreenLayout({super.key});
-  
+  const MobileScreenLayout({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return  DefaultTabController(
-      length: 3, 
+    return DefaultTabController(
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: appBarColor,
           elevation: 0,
-          title: const Text('WhatsApp',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-           ),
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           actions: [
-                IconButton(onPressed: (){},
-           icon:const Icon(Icons.camera_alt_outlined),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.camera_alt_outlined),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+            )
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(text: 'Chats'),
+              Tab(text: 'Status'),
+              Tab(text: 'Calls'),
+            ],
+          ),
         ),
-            IconButton(onPressed: (){},
-           icon:const Icon(Icons.search),
+        body:const  TabBarView(
+          children: [
+            ContactsList(), 
+            StatusScreen(), 
+            CallScreen(),
+
+            
+          ],
         ),
-            IconButton(onPressed: (){},
-           icon:const Icon(Icons.more_vert),
-        )
-        ],
-        bottom:const TabBar(
-          indicatorColor: tabColor,
-          indicatorWeight: 4,
-          labelColor: tabColor,
-          unselectedLabelColor: Colors.grey,
-          tabs:[
-          // Tab(icon: Icon(Icons.groups_2),),
-          Tab(text: 'Chats',),
-          Tab(text: 'Status',),
-          Tab(text: 'Calls',),
-        ]),
-        ),
-        body: const ContactsList(),
         floatingActionButton: FloatingActionButton(
-          
-          onPressed: (){},
+          onPressed: () {},
           backgroundColor: tabColor,
-          child:const Icon(Icons.comment, color: Colors.white, ),
+          child: const Icon(Icons.comment, color: Colors.white),
         ),
-      )
-      );
+      ),
+    );
+  }
+}
+
+// Example StatusScreen widget (you can create a separate file for this)
+class StatusScreen extends StatelessWidget {
+  const StatusScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Status Screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class CallScreen extends StatelessWidget {
+  const CallScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'call-screen',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
   }
 }
